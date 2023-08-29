3b229b647df3448862760a8df636b01f270f7461
MohammadAhmadShaaban <eng.mashaaban@yahoo.com>
2023-08-29 16:03:48 +0300

    add all files

---
+++ Controllers/CategotriesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication31.Models;
+
+namespace WebApplication31.Controllers
+{
+    public class CategotriesController : Controller
+    {
+        private Training1Entities db = new Training1Entities();
+
+        // GET: Categotries
+        public ActionResult Index()
+        {
+            return View(db.Categotries.ToList());
+        }
+
+        // GET: Categotries/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categotry categotry = db.Categotries.Find(id);
+            if (categotry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categotry);
+        }
+
+        // GET: Categotries/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categotries/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CategoryID,CategoryName,CategorySalePrice,CategoryBuyPrice")] Categotry categotry)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categotries.Add(categotry);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(categotry);
+        }
+
+        // GET: Categotries/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categotry categotry = db.Categotries.Find(id);
+            if (categotry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categotry);
+        }
+
+        // POST: Categotries/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CategoryID,CategoryName,CategorySalePrice,CategoryBuyPrice")] Categotry categotry)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(categotry).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(categotry);
+        }
+
+        // GET: Categotries/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categotry categotry = db.Categotries.Find(id);
+            if (categotry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categotry);
+        }
+
+        // POST: Categotries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Categotry categotry = db.Categotries.Find(id);
+            db.Categotries.Remove(categotry);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/SalesController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication31.Models;
+
+namespace WebApplication31.Controllers
+{
+    public class SalesController : Controller
+    {
+        private Training1Entities db = new Training1Entities();
+
+        // GET: Sales
+        public ActionResult Index()
+        {
+            var sales = db.Sales.Include(s => s.Categotry);
+            return View(sales.ToList());
+        }
+
+
+        public ActionResult profit()
+        {
+            var sales = db.Sales.Include(s => s.Categotry).ToList();
+            var total = 0.0;
+            for (var i = 0; i < sales.Count(); i++)
+            {
+                total = (double)(total + (sales[i].Categotry.CategorySalePrice - sales[i].Categotry.CategoryBuyPrice) * sales[i].Quantity);
+            }
+            ViewBag.TotalProfit = total;
+            return View(sales);
+        }
+        // GET: Sales/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sale sale = db.Sales.Find(id);
+            if (sale == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sale);
+        }
+
+        // GET: Sales/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryID = new SelectList(db.Categotries, "CategoryID", "CategoryName");
+            return View();
+        }
+
+        // POST: Sales/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SalesID,CategoryID,Quantity")] Sale sale)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Sales.Add(sale);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryID = new SelectList(db.Categotries, "CategoryID", "CategoryName", sale.CategoryID);
+            return View(sale);
+        }
+
+        // GET: Sales/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sale sale = db.Sales.Find(id);
+            if (sale == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryID = new SelectList(db.Categotries, "CategoryID", "CategoryName", sale.CategoryID);
+            return View(sale);
+        }
+
+        // POST: Sales/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SalesID,CategoryID,Quantity")] Sale sale)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sale).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryID = new SelectList(db.Categotries, "CategoryID", "CategoryName", sale.CategoryID);
+            return View(sale);
+        }
+
+        // GET: Sales/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sale sale = db.Sales.Find(id);
+            if (sale == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sale);
+        }
+
+        // POST: Sales/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sale sale = db.Sales.Find(id);
+            db.Sales.Remove(sale);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
